52ccfdbf322f045eadc9245385eee2a55619e600
Shea Knox <Shea Knox>
2017-03-26 17:31:19 -0500

    changed questions and timer to objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,113 @@
-//Display a single button on the screen that will start the game.
-	//When this button is clicked it should hide itself and reveal the timer and question/answers.
+var questions = {
+	q1: "What color is the sky?",
+	q2: "How many planets are in our solar system?",
+	q3: "How many letters are in the alphabet?",
+	a1: ["blue", "red", "green"],
+	a2: ["8", "7", "9"],
+	a3: ["26", "24", "27"]
+}
 
-var questions = [["What color is the sky?", "blue"], ["How many planets are in our solar system?", "8"], ["How many letters are in the alphabet?", "26"]]
-var time = 5;
-var intervalId;
+var interval;
+var answer;
 
-$("button").on("click", function(){
-	$(this).hide();
-	startGame();	
-}); //end start button click function
+var timer = {
+	time: 6,
+	decrement: function(){
+    	timer.time--;
+    	$("#timer").html("Time Remaining:" + timer.time);
+    	if(timer.time === 0){
+      	timer.stop();
+      	$("#question").html("<h1>Time's up!</h2>")
+    	}
+    },
+	start: function() {
+      interval = setInterval(timer.decrement, 1000)
+      
+  	},
+    stop: function(){
+    	clearInterval(interval)
+    },
+    reset: function(){
+    	timer.time = 6;
+    },
+};//end timer{}
 
+function question1(){
+	$("#question").html(questions.q1 + "<br><br>"
+		+ "<button>" + questions.a1[0] + "</button><br><br>"
+		+ "<button>" + questions.a1[1] + "</button><br><br>"
+		+ "<button>" + questions.a1[2] + "</button><br><br>"
+		)
+	$("button").on("click", function(){
+		if($(this).text() === "blue"){
+			answer = true;
+		} else{
+			answer = false;
+		}
+		timer.stop();
+		timer.reset();
+		answerMessage();
+		question2()
+	});//end button click
+};//end question3()
 
-function questionOne(){
-	$("#question").html("<h2>" + questions[0][0] + "</h2>" + "<br><br>"
-		+ "<button>red</button><br><br>"
-		+ "<button>blue</button><br><br>"
-		+ "<button>green</button><br><br>"
-		);
-};//end questionOne
+function question2(){
+	$("#question").html(questions.q2 + "<br><br>"
+		+ "<button>" + questions.a2[1] + "</button><br><br>"
+		+ "<button>" + questions.a2[0] + "</button><br><br>"
+		+ "<button>" + questions.a2[2] + "</button><br><br>"
+		)
+	timer.start();
+	$("button").on("click", function(){
+		if($(this).text() === "8"){
+			answer = true;
+		} else{
+			answer = false;
+		}
+		timer.stop();
+		answerMessage();
+		question3();
+	});//end button click
+};//end question3()
 
-function questionTwo(){
-	$("#question").html("<h2>" + questions[1][0] + "</h2>" + "<br><br>"
-		+ "<button>4</button><br><br>"
-		+ "<button>2</button><br><br>"
-		+ "<button>8</button><br><br>"
-		);
-};//end questionTwo
+function question3(){
+	$("#question").html(questions.q3 + "<br><br>"
+		+ "<button>" + questions.a3[2] + "</button><br><br>"
+		+ "<button>" + questions.a3[1] + "</button><br><br>"
+		+ "<button>" + questions.a3[0] + "</button><br><br>"
+		)
+	timer.start();
+	$("button").on("click", function(){
+		if($(this).text() === "26"){
+			answer = true;
+		} else{
+			answer = false;
+		}
+		timer.stop();
+		answerMessage();
+	});//end button click
+};//end question3()
 
-function questionThree(){
-	$("#question").html("<h2>" + questions[2][0] + "</h2>" + "<br><br>"
-		+ "<button>15</button><br><br>"
-		+ "<button>21</button><br><br>"
-		+ "<button>26</button><br><br>"
-		);
-};//end questionThree
-
-function startGame(){
-	startTime();
-	questionOne();
-	if(time === 0){
-		startTime();
-		questionTwo();
+function answerMessage (){
+	if(answer === true){
+		$("#question").html("<h1>You got it!</h2>")
+	} else{
+		$("#question").html("<h1>Wrong answer!</h2>")
 	}
-};
+};//end answerMessage()
 
-function startTime() {
-	  time = 5;
-      intervalId = setInterval(decrement, 1000);
-      clearInterval(intervalId);
-};
+function startTrivia(){
+	question1();
+	timer.start();
+};//end startTrivia()
 
-    
-function decrement() {
-      time--;
-      $("#timer").html("<h2>Time Left: " + time + "</h2>");
-};
+$("#button").on("click", function(){
+	$(this).hide();
+	startTrivia();
 
+}); //end #button click function
 
 
 
-
-console.log("hello");
 
 
